4fb9cb840208f3e455b0ea00314392b6dc054111
Mihai Sturza <sturzamimi@gmail.com>
2021-04-28 19:13:15 +0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicInputComponent } from './components/basic-input/basic-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/basic-input/basic-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lateral-basic-input',
+  templateUrl: './basic-input.component.html',
+  styleUrls: ['./basic-input.component.scss']
+})
+export class BasicInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
